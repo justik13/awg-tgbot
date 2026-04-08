f3165fa8a9e8a4eb55bac0e9bb9e03b986eee933
justik13 <honorplay2100@gmail.com>
2026-04-08 23:57:57 +0500

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault('API_TOKEN', '123:abc')
-os.environ.setdefault('ADMIN_ID', '1')
-os.environ.setdefault('SERVER_PUBLIC_KEY', 'pubkey')
-os.environ.setdefault('SERVER_IP', '8.8.8.8:51820')
-os.environ.setdefault('ENCRYPTION_SECRET', 'secretsecretsecretsecret')
